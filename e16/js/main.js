1c28ae905b8183fceca94ef375342873bf7b565d
zkochaji <46933911+zkochaji@users.noreply.github.com>
2019-04-02 13:00:51 -0400

    e16

---
+++ e16/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  $('#number5').click(function() {
+      $('#box5').css('background-color','#FFC300');
+      $('#number5').toggle();
+      $('#number5p').toggle();
+      $('#number5').fadeout();
+  });
+  $('#number4').click(function() {
+      $('#box4').css('background-color','#FFC300');
+      $('#number4').toggle();
+      $('#number4p').toggle();
+      $('#number4').fadeout();
+  });
+  $('#number3').click(function() {
+      $('#box3').css('background-color','#FFC300');
+      $('#number3').toggle();
+      $('#number3p').toggle();
+      $('#number3').fadeout();
+  });
+  $('#number2').click(function() {
+      $('#box2').css('background-color','#FFC300');
+      $('#number2').toggle();
+      $('#number2p').toggle();
+      $('#number2').fadeout();
+  });
+  $('#number1').click(function() {
+      $('#box1').css('background-color','#FFC300');
+      $('#number1').toggle();
+      $('#number1p').toggle();
+      $('#number1').fadeout();
+  });
+
+
+  $('#number5p').click(function() {
+      $('#box5').css('background-color','#222629');
+      $('#number5').toggle();
+      $('#number5p').toggle();
+      $('#number5p').fadeout();
+  });
+  $('#number4p').click(function() {
+      $('#box4').css('background-color','#222629');
+      $('#number4').toggle();
+      $('#number4p').toggle();
+      $('#number4p').fadeout();
+  });
+  $('#number3p').click(function() {
+      $('#box3').css('background-color','#222629');
+      $('#number3').toggle();
+      $('#number3p').toggle();
+      $('#number3p').fadeout();
+  })
+  $('#number2p').click(function() {
+      $('#box2').css('background-color','#222629');
+      $('#number2').toggle();
+      $('#number2p').toggle();
+      $('#number2p').fadeout();
+  });
+  $('#number1p').click(function() {
+      $('#box1').css('background-color','#222629');
+      $('#number1').toggle();
+      $('#number1p').toggle();
+      $('#number1p').fadeout();
+  });
+});
