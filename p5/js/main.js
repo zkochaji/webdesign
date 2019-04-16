60169c43256b03ef2032881615f06cbcad4a5c7d
zkochaji <46933911+zkochaji@users.noreply.github.com>
2019-04-16 16:30:46 -0400

    p5

--- p5/js/main.js
+++ p5/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $('#button').click(function() {
-      $('body').css('background-color','red');
+  var list = $("ul li").toArray();
+  var length = list.length;
+   $('#button').click(function(){
+     var randomnum = Math.floor(Math.random()*length);
+     var i;
+     for (i = 0; i < length; i++) {
+     $('#img' + i).css("display", "none");
+     console.log(length);
+     }
+   var randomitem = 'img'+randomnum;
+   $("#" + randomitem).css("display", "block");
+   console.log(randomitem);
   });
- 
+});
